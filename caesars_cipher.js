8a4176f8b5fcfaf401ec72a3dbb6d4376aacb3e9
bobbypt05 <bobbypt05@gmail.com>
2018-04-29 20:55:23 +0530

    Final code Updated

--- caesars_cipher.js
+++ caesars_cipher.js
@@ -1,5 +1,6 @@
-//This code is incomplete..
-
+//One of the simplest and most widely known ciphers is a Caesar cipher, 
+//also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+//Do  ROT13 cipher(shift each charachter by 13)
 
 
 function rot13(str) {
@@ -9,19 +10,17 @@ function rot13(str) {
   
   for(var i=0;i<str.length;i++)
   {
-         values.push(str.charCodeAt(str[i])); 
+		values.push(str.charCodeAt(i)); 
   }
-  
-  console.log(values);
-  
+
   for(var j=0;j<str.length;j++)
   {
-        if(values[j]>64 && values[j]<92)
+        if(values[j]>64 && values[j]<91)
         {
               values[j]+=13;
-              if(values[j]>91)
+              if(values[j]>90)
               {
-                  values[j] = values[j]%92+65;    
+                  values[j] = values[j]%91+65;    
               }
         }
   }
